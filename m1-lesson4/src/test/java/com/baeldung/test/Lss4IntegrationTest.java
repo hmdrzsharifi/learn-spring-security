43f2bbb51800641b90367d9f4d70f8de235f1d6b
sharifi <h_sharifi@modernisc.com>
2021-04-22 01:14:05 +0430

    add test for protected resource: "/delete/**"

--- m1-lesson4/src/test/java/com/baeldung/test/Lss4IntegrationTest.java
+++ m1-lesson4/src/test/java/com/baeldung/test/Lss4IntegrationTest.java
@@ -1,15 +1,45 @@
 package com.baeldung.test;
 
 import com.baeldung.lss.spring.LssApp4;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = LssApp4.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class Lss4IntegrationTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void givenRequestOnPrivateResource_thenFailWith401() throws Exception {
+        mvc.perform(get("/delete/baeldung")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
 
     @Test
     public void whenLoadApplication_thenSuccess() {
